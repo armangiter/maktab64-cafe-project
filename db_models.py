f79988477e8104b36206fe82131311c91e26cb7b
mahdizo <mahdizolfaghari3181@gmail.com>
2021-12-30 00:23:17 +0330

    fix menu_items

--- db_models.py
+++ db_models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, String, create_engine, Integer, DateTime
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, foreign
 
 Base = declarative_base()
 engine = create_engine('postgres://lkfrpdyj:hhUJANbdSKxjUZFbO1Ph0E_R4D4wBhT0@castor.db.elephantsql.com/lkfrpdyj')
@@ -12,7 +12,7 @@ class Menu_Items(Base):
     id = Column('Id', Integer, unique=True, primary_key=True)
     name = Column('Name', String, unique=True)
     price = Column('Price', Integer, unique=False)
-    category = Column('Category', String, unique=False)
+    category = Column('Category', String, unique=False, foreign_key='category.id')
     discount = Column('discount', Integer, unique=False)
     serv_time = Column('ServTime', Integer, unique=False)
     st_cooking_time = Column('StCookingTime', Integer)
