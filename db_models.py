2bd89072209f77629544b393c749d55ab733468b
mahdizo <mahdizolfaghari3181@gmail.com>
2021-12-29 23:54:32 +0330

    feat menu_items

--- db_models.py
+++ db_models.py
@@ -3,5 +3,19 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
-engine = create_engine('postgresql://postgres:mahdi12345M@localhost/flash_cards')
+engine = create_engine('postgres://lkfrpdyj:hhUJANbdSKxjUZFbO1Ph0E_R4D4wBhT0@castor.db.elephantsql.com/lkfrpdyj')
 session = sessionmaker(bind=engine)()
+
+
+class Menu_Items(Base):
+    __tablename__ = 'menu'
+    id = Column('Id', Integer, unique=True, primary_key=True)
+    name = Column('Name', String, unique=True)
+    price = Column('Price', Integer, unique=False)
+    category = Column('Category', String, unique=False)
+    discount = Column('discount', Integer, unique=False)
+    serv_time = Column('ServTime', Integer, unique=False)
+    st_cooking_time = Column('StCookingTime', Integer)
+
+
+Base.metadata.create_all(engine)
