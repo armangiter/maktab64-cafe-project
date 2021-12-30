963aa656d4399a06acfc2e7e085ee51724124c98
mahdizo <mahdizolfaghari3181@gmail.com>
2021-12-30 20:23:39 +0330

    feat recipts class

--- db_models.py
+++ db_models.py
@@ -1,20 +1,27 @@
-from sqlalchemy import Column, String, create_engine, Integer, DateTime
+from sqlalchemy import Column, ForeignKey, String, create_engine, Integer, DateTime
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, foreign
+from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 engine = create_engine('postgres://lkfrpdyj:hhUJANbdSKxjUZFbO1Ph0E_R4D4wBhT0@castor.db.elephantsql.com/lkfrpdyj')
 session = sessionmaker(bind=engine)()
 
 
-class Cashier(Base):
-    __tablename__ = 'Cashier'
+#
+# class Cashier(Base):
+#     __tablename__ = 'Cashier'
+#     id = Column('id', Integer, unique=True, primary_key=True)
+#     firstname = Column('firstname', String)
+#     lastname = Column('lastname', String)
+#     phone = Column('phone', String, unique=True)
+#     email = Column('email', String, unique=True)
+#     password = Column('password', String)
+
+class Category(Base):
+    __tablename__ = "Category"
     id = Column('id', Integer, unique=True, primary_key=True)
-    firstname = Column('firstname', String)
-    lastname = Column('lastname', String)
-    phone = Column('phone', String, unique=True)
-    email = Column('email', String, unique=True)
-    password = Column('password', String)
+    title = Column('title', String)
+    root = Column('root', String, default=None)
 
 
 class Menu_Items(Base):
@@ -22,7 +29,7 @@ class Menu_Items(Base):
     id = Column('Id', Integer, unique=True, primary_key=True)
     name = Column('Name', String, unique=True)
     price = Column('Price', Integer, unique=False)
-    category = Column('Category', String, unique=False, foreign_key='Category.id')
+    category = Column('Category', Integer, ForeignKey('Category.id', ondelete='CASCADE'))
     discount = Column('discount', Integer, unique=False, default=0)
     serv_time = Column('ServTime', Integer, unique=False, default=10)
     st_cooking_time = Column('StCookingTime', Integer, default=10)
@@ -36,20 +43,23 @@ class Table(Base):
     capacity = Column('capacity', Integer, unique=True, default=2)
 
 
-class Category(Base):
-    __tablename__ = "Category"
-    id = Column('id', Integer, unique=True, primary_key=True)
-    title = Column('title', String)
-    root = Column('root', String, default=None)
-
-
 class Orders(Base):
     __tablename__ = "Orders"
     id = Column('id', Integer, unique=True, primary_key=True)
-    table_id = Column('table_id', Integer, unique=True, foreign_key='Table.id')
+    table_id = Column('table_id', Integer, ForeignKey('Table.id', ondelete='CASCADE'))
     number = Column('number', Integer)
     status = Column('status', String)
     time_stamp = Column('time_stamp', DateTime)
+
+
+class Receipts(Base):
+    __tablename__ = "Receipts"
+    id = Column('id', Integer, unique=True, primary_key=True)
+    table_id = Column('table_id', Integer, ForeignKey('Table.id', ondelete='CASCADE'))
+    total_price = Column('total_price', Integer, default=0)
+    final_price = Column('final_price', Integer, default=0)
+    time_stamp = Column('time_stamp', DateTime)
+
 
 
 Base.metadata.create_all(engine)
