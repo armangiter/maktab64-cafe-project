299512e50f6489efb30249092170385e73b0faba
mahdizo <mahdizolfaghari3181@gmail.com>
2021-12-30 19:14:14 +0330

    refactore : db_models

--- core/db_config.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import  create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-
-Base = declarative_base()
-engine = create_engine('postgres://lkfrpdyj:hhUJANbdSKxjUZFbO1Ph0E_R4D4wBhT0@castor.db.elephantsql.com/lkfrpdyj')
-session = sessionmaker(bind=engine)()
