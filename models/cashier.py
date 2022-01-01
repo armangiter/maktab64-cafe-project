3930b41598bea29f688c69138da9dab918f16182
alireza <alirezahn1@yahoo.com>
2022-01-01 19:56:34 +0330

    fix cashier class

--- models/cashier.py
+++ models/cashier.py
@@ -1,17 +1,5 @@
 import db_models
 from db_models import *
-from json import dumps
-
-
-
-class CashierError(Exception):
-    def __init__(self, msg: str, field: str, data: ...) -> None:
-        super().__init__(msg, field, data)
-        self.msg = msg
-        self.field = field
-        self.data = data
-    def __str__(self):
-        return f"error on field `{self.field}` (invalid data: `{self.data}`): {self.msg}"
 
 class Cashier(Base):
     def __init__(self, first_name, last_name, phone_number, password, email=None, **extra_information):
@@ -21,7 +9,8 @@ class Cashier(Base):
         self.phone_number = phone_number[-9:]
         self.email = email
         self.password = password
-        self.extra_information = dumps(extra_information)
 
         cashier = db_models.Cashier(first_name=self.first_name,last_name=self.last_name,phone_number=self.phone_number
                                     ,password=self.password,email=self.email)
+        session.add(cashier)
+        session.commit()
