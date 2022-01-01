aa690494187c4d197ed045d459c52aeacba19856
Ghodratijafar <jamalgh1376.jg@gmail.com>
2022-01-01 18:19:53 +0330

    refactor class receipts

--- models/receipts.py
+++ models/receipts.py
@@ -1,18 +1,35 @@
+from table import *
+from db_models import *
+from datetime import datetime
 
-class Receipts:
 
-    def __init__(self, table_id, time_stamp, total_price=0, final_price=0):
+class Receipts_model:
+
+    def __init__(self, table_id, total_price=0, final_price=0):
         self.table_id = table_id
         self.total_price = total_price
         self.final_price = final_price
-        self.time_stamp = time_stamp
+        self.time_stamp = datetime.now()
 
-    def add(self):
-        pass
+    def create(self):
+        new_row = Receipts(table_id=self.table_id, total_price=self.total_price, final_price=self.final_price,
+                           time_stamp=self.time_stamp)
+        session.add(new_row)
+        session.commit()
 
     def show_all(self):
+        receipts = session.query(Receipts).all()
+        for i in receipts:
+            return i
+
+    @classmethod
+    def delete(cls, table_id):
+        session.query(Receipts).filter(Receipts.table_id == table_id).delete()
+        session.commit()
+
+    @classmethod
+    def update(cls):
         pass
 
-    def delete(self):
-        pass
+
 
