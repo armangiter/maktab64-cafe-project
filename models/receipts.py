2c18e32afd5fbf61034d05fd372a03a9159ef47e
Ghodratijafar <jamalgh1376.jg@gmail.com>
2022-01-01 16:19:08 +0330

    feat class receipts

--- models/receipts.py
+++ models/receipts.py
@@ -1,2 +1,18 @@
 
+class Receipts:
+
+    def __init__(self, table_id, time_stamp, total_price=0, final_price=0):
+        self.table_id = table_id
+        self.total_price = total_price
+        self.final_price = final_price
+        self.time_stamp = time_stamp
+
+    def add(self):
+        pass
+
+    def show_all(self):
+        pass
+
+    def delete(self):
+        pass
 
