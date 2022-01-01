8a44dd78755d8a0070fda4dbdc6054bd6981e745
alireza <alirezahn1@yahoo.com>
2022-01-01 18:42:25 +0330

    feat cashier error class

--- models/cashier.py
+++ models/cashier.py
@@ -3,6 +3,13 @@ from json import dumps, loads
 
 
 
+class CashierError(Exception):
+    def __init__(self, msg: str, field: str, data: ...) -> None:
+        super().__init__(msg, field, data)
+        self.msg = msg
+        self.field = field
+        self.data = data
+
 class Cashier(db_models.Base):
     def __init__(self, first_name, last_name, phone_number, password, email=None, **extra_information):
 
