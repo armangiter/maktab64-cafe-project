becb0eb0ce95537a0b6db5e315caef03d57ff0d3
alireza <alirezahn1@yahoo.com>
2022-01-01 17:44:23 +0330

    feat cashier class

---
+++ models/cashier.py
@@ -0,0 +1,14 @@
+from .. import db_models
+from json import dumps, loads
+
+
+
+class Cashier(db_models.Base):
+    def __init__(self, first_name, last_name, phone_number, password, email=None, **extra_information):
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = phone_number[-9:]
+        self.email = email
+        self.password = password
+        self.extra_information = dumps(extra_information)
