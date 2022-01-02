e6f6cf90e2ea2929aa5d898bba7991cef50389f4
arman <mr.kazemi.ar@gmail.com>
2022-01-03 03:10:35 +0330

    refactor : table.py delete creat method

--- models/table.py
+++ models/table.py
@@ -7,8 +7,6 @@ class TableModels:
         self.table_number = table_number
         self.cafe_position = cafe_position
         self.capacity = capacity
-
-    def create(self):
         new_row = Table(table_number=self.table_number, cafe_position=self.cafe_position,
                         capacity=self.capacity)
         session.add(new_row)
@@ -18,3 +16,4 @@ class TableModels:
     def delete(cls, table_id):
         session.query(Table).filter(Table.table_number == table_id).delete()
         session.commit()
+
