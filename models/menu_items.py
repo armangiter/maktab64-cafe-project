4d08b297cb9a95786959daafa31ee460c58748de
arman <mr.kazemi.ar@gmail.com>
2022-01-02 15:07:55 +0330

    feat : menu items attrs

--- models/menu_items.py
+++ models/menu_items.py
@@ -1,3 +1,11 @@
 from db_models import *
 
+
 class MenuItems:
+    def __init__(self, name, price, category, discount=0, serv_time=20, st_cooking_time=20):
+        self.name = name
+        self.price = price
+        self.category = category
+        self.discount = discount
+        self.serv_time = serv_time
+        self.st_cooking_time = st_cooking_time
