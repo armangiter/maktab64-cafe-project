98f72a7472b75668a4bcb3f74eba2c0f1623c36a
arman <mr.kazemi.ar@gmail.com>
2022-01-03 01:21:13 +0330

    fix : reciepts.py init method

--- models/reciepts.py
+++ models/reciepts.py
@@ -1,15 +1,14 @@
-from table import *
 from db_models import *
 from datetime import datetime
 
 
-class Receipts_model:
+class Receipts:
 
-    def __init__(self, table_id, total_price=0, final_price=0):
+    def __init__(self, table_id, total_price=0, final_price=0, time_stamp=datetime.now()):
         self.table_id = table_id
         self.total_price = total_price
         self.final_price = final_price
-        self.time_stamp = datetime.now()
+        self.time_stamp = time_stamp
 
     def create(self):
         new_row = Receipts(table_id=self.table_id, total_price=self.total_price, final_price=self.final_price,
