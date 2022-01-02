1cf4f5a366bea69af043c8c7a6ae587a635743fe
arman <mr.kazemi.ar@gmail.com>
2022-01-02 09:18:56 +0330

    refactor:orders attr

--- models/orders.py
+++ models/orders.py
@@ -1,5 +1,11 @@
 from db_models import *
 
+
 class Orders:
 
-    def __init__(self,id,table_id,number,status,time_stamp):
+    def __init__(self, id, table_id, number, status):
+        self.id = id
+        self.table_id = table_id
+        self.number = number
+        self.status = status
+        self.time_stamp=
