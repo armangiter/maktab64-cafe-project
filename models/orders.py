5d467d8333dd70ec6855380fe889ae4d46a9fdf6
arman <mr.kazemi.ar@gmail.com>
2022-01-02 09:27:16 +0330

    fix : orders init attrs

--- models/orders.py
+++ models/orders.py
@@ -4,13 +4,12 @@ from datetime import datetime
 
 class Orders:
 
-    def __init__(self, id, table_id, number, status):
+    def __init__(self, id, table_id, number):
         self.id = id
         self.table_id = table_id
         self.number = number
-        self.__status = status
+        self.__status = 'ordered'
         self.time_stamp = datetime.now()
 
     def change_status(self, stat="ordered"):
         self.__status = stat
-
