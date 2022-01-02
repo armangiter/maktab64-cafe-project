b3446d3ed79a8033b1a51498c2a66440989c0ca4
arman <mr.kazemi.ar@gmail.com>
2022-01-02 16:21:26 +0330

    fix : table models some names

--- models/table.py
+++ models/table.py
@@ -3,19 +3,18 @@ from db_models import *
 
 class TableModels:
 
-    def __init__(self, id, table_number, cafe_position, capacity):
-        self.id = id
+    def __init__(self, table_number, cafe_position, capacity):
         self.table_number = table_number
         self.cafe_position = cafe_position
         self.capacity = capacity
 
     def create(self):
-        new_row = Table(id=self.id, table_number=self.table_number, cafe_position=self.cafe_position,
+        new_row = Table(table_number=self.table_number, cafe_position=self.cafe_position,
                         capacity=self.capacity)
         session.add(new_row)
         session.commit()
 
     @classmethod
-    def delete(cls, table_number):
-        session.query(Table).filter(Table.table_number == table_number).delete()
+    def delete(cls, table_id):
+        session.query(Table).filter(Table.table_number == table_id).delete()
         session.commit()
