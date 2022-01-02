00a329cacf637ce4a76fc413285d302d4ab00a78
arman <mr.kazemi.ar@gmail.com>
2022-01-02 15:26:36 +0330

    fix : db_models method order_item 'order_item' > "order_id"

--- models/order_item.py
+++ models/order_item.py
@@ -5,3 +5,4 @@ class OrderItem:
     def __init__(self, order_id, item_id):
         self.order_id = order_id
         self.menu_id = item_id
+        new_row=Order_items(order_id=self.order_id,item_id)
