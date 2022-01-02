4e634d2e0a4d6dfffef99461a31fb42115248ab6
arman <mr.kazemi.ar@gmail.com>
2022-01-02 15:29:13 +0330

    feat : order item database connection

--- models/order_item.py
+++ models/order_item.py
@@ -5,4 +5,6 @@ class OrderItem:
     def __init__(self, order_id, item_id):
         self.order_id = order_id
         self.item_id = item_id
-        new_row=Order_items(order_id=self.order_id,item_id=self.)
+        new_row = Order_items(item_id=self.item_id, order_id=self.order_id)
+        session.add(new_row)
+        session.commit()
