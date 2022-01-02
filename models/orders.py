1e29eed9165cdf6bda5b762677b0bc4570cfb02d
arman <mr.kazemi.ar@gmail.com>
2022-01-02 10:27:44 +0330

    fix : orders init for database connection

--- models/orders.py
+++ models/orders.py
@@ -2,13 +2,16 @@ from db_models import *
 from datetime import datetime
 
 
-class Orders:
+class Order:
 
     def __init__(self, table_id, number):
         self.table_id = table_id
         self.number = number
         self.__status = 'ordered'
-        self.time_stamp = datetime.now()
+        new_row = Orders(table_id=self.table_id, number=self.number,
+                         status=self.get_status, time_stamp=datetime.now())
+        session.add(new_row)
+        session.commit()
 
     def change_status(self, stat="ordered"):
         if self.__status:
@@ -16,3 +19,4 @@ class Orders:
 
     def finish(self):
         self.__status = False
+
