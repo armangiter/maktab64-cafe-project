e53f6299eb18b067f7df16db51594de041aa4606
arman <mr.kazemi.ar@gmail.com>
2022-01-04 02:36:46 +0330

    feat : rout menu

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from views import clientviews, admin
 
 app = Flask(__name__)
 
-app.add_url_rule('/', 'home', clientviews.index)
+app.add_url_rule('/', 'home', clientviews.home)
+app.add_url_rule('/menu', 'menu', clientviews.menu)
 if __name__ == '__main__':
     app.run()
