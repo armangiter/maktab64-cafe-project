39c814efe5485770601f62fdf8c149215b977684
alireza <alirezahn1@yahoo.com>
2022-01-03 13:10:13 +0330

    feat base_variable

---
+++ views/clientviews.py
@@ -0,0 +1,12 @@
+from flask import render_template
+
+
+base_variables = {
+    "page": {
+        "base_title": "cafe",
+        "lang": 'en-US',
+        "title": 'Cafe'
+    },
+
+    "links": ["INDEX","HOME", "ABOUT", "MENU", "TEAM"]
+}
