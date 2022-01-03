6bc7819649d1e62f7086eefaf5c90ed9b040a30d
arman <mr.kazemi.ar@gmail.com>
2022-01-03 04:03:28 +0330

    feat: admin.py register view

--- views/admin.py
+++ views/admin.py
@@ -8,8 +8,16 @@ def login():
     if request.method == "POST":
         cashier_dict = cashier.CashierModels.all_cashiers()
         for c in cashier_dict:
-            if request.form.get('phone') == c['phone'] and request.form.get('password') == c['password']:
+            if escape(request.form.get('phone')) == c['phone'] and \
+                    escape(request.form.get('password')) == c['password']:
                 return render_template('admin_page', data=c)
     elif request.method == 'GET':
         return render_template('register')
 
+
+def register():
+    if request.method == 'POST':
+        cashier.CashierModels(escape(request.form.get('firstname')), escape(request.form.get('lastname')),
+                              escape(request.form.get('phone')),
+                              escape(request.form.get('password')), escape(request.form.get('email')))
+        return render_template('login')
