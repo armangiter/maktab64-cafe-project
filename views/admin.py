ee4ac58e8786ca22b42c4a20440c9b8e974c493d
arman <mr.kazemi.ar@gmail.com>
2022-01-03 03:54:44 +0330

    feat: login view

--- views/admin.py
+++ views/admin.py
@@ -1,8 +1,15 @@
 from models import *
 from flask import Flask
 from flask import redirect, url_for, request, render_template, escape, render_template_string, Response
+from models import cashier, category, menu_items, order_item, orders, table, reciepts
 
 
 def login():
-    pass
+    if request.method == "POST":
+        cashier_dict = cashier.CashierModels.all_cashiers()
+        for c in cashier_dict:
+            if request.form.get('phone') == c['phone'] and request.form.get('password') == c['password']:
+                return render_template('admin_page', data=c)
+    elif request.method == 'GET':
+        return render_template('register')
 
