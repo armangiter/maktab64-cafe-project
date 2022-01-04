a08d467f802e757bec0428f77fdaa7014e29f1bf
arman <mr.kazemi.ar@gmail.com>
2022-01-04 03:33:31 +0330

    feat : html form

--- app.py
+++ app.py
@@ -5,5 +5,6 @@ app = Flask(__name__)
 
 app.add_url_rule('/', 'home', clientviews.home)
 app.add_url_rule('/menu', 'menu', clientviews.menu)
+app.add_url_rule('/menuitem', 'menuitem', admin.menu_item, methods=['GET', 'POST'])
 if __name__ == '__main__':
     app.run()
