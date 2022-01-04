ed1a77ec600d37167a32480e5d27ed87fdee38a6
arman <mr.kazemi.ar@gmail.com>
2022-01-04 12:03:34 +0330

    feat : app.url_role

--- app.py
+++ app.py
@@ -6,5 +6,6 @@ app = Flask(__name__)
 app.add_url_rule('/', 'home', clientviews.home)
 app.add_url_rule('/menu', 'menu', clientviews.menu)
 app.add_url_rule('/menuitem', 'menuitem', admin.menu_item, methods=['GET', 'POST'])
+app.add_url_rule('/register', 'register', admin.register, methods=['GET', 'POST'])
 if __name__ == '__main__':
     app.run()
