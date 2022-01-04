3d449345abe53774248cdf8c242e4ce693814a30
arman <mr.kazemi.ar@gmail.com>
2022-01-04 13:12:11 +0330

    fix : category rout

--- app.py
+++ app.py
@@ -10,6 +10,6 @@ app.add_url_rule('/register', 'register', admin.register, methods=['GET', 'POST'
 app.add_url_rule('/login', 'login', admin.login, methods=['GET', 'POST'])
 app.add_url_rule('/adminpage', 'adminpage', admin.admin_page, methods=['GET', 'POST'])
 app.add_url_rule('/orders', 'orders', admin.order, methods=['GET', 'POST'])
-app.add_url_rule('/category','category',admin.)
+app.add_url_rule('/category', 'category', admin.categories, methods=['GET', 'POST'])
 if __name__ == '__main__':
     app.run()
