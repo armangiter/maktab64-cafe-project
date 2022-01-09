25552d06a712a7e08db2da31ce73af121b1e6dde
arman <mr.kazemi.ar@gmail.com>
2022-01-09 17:05:22 +0330

    fix : import files

--- core/manager.py
+++ core/manager.py
@@ -1,10 +1,12 @@
 from sqlalchemy import Column, ForeignKey, String, create_engine, Integer, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from abc import ABC, abstractmethod
+
 
 Base = declarative_base()
 engine = create_engine('postgresql://postgres:123456@localhost/postgres')
 session = sessionmaker(bind=engine)()
 
 
-class BaseManager(Base):
+
