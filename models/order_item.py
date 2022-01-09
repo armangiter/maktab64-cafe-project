b1e39d57b3be8724ad7e2a852141530dd0917754
arman <mr.kazemi.ar@gmail.com>
2022-01-09 17:58:33 +0330

    feat : read method in order_item.py

--- models/order_item.py
+++ models/order_item.py
@@ -1,13 +1,19 @@
 from core.db_models import *
+from core.manager import BaseManager
 
 
-class OrderItem:
+class OrderItem(BaseManager):
     def __init__(self, order_id, item_id):
         self.order_id = order_id
         self.item_id = item_id
         new_row = Order_items(item_id=self.item_id, order_id=self.order_id)
         session.add(new_row)
         session.commit()
+
+    @classmethod
+    def read(cls, row_id):
+        data = session.query(Order_items).filter(Order_items.id == row_id)
+        return data
 
     @classmethod
     def all_order_items(cls):
