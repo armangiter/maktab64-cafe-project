60003e4e8c827b5dd1fe61d7a5049e8fbe24386d
arman <mr.kazemi.ar@gmail.com>
2022-01-09 17:09:26 +0330

    feat : create method

--- core/manager.py
+++ core/manager.py
@@ -3,10 +3,15 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from abc import ABC, abstractmethod
 
-
 Base = declarative_base()
 engine = create_engine('postgresql://postgres:123456@localhost/postgres')
 session = sessionmaker(bind=engine)()
 
 
-
+class BaseManager(ABC):
+    @classmethod
+    @abstractmethod
+    def create(cls, table: str, row_id):
+        """
+        to create an object and save into data base
+        """
