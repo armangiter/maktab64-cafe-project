ed0935c4152a11332622e18d7f0414d05521abaf
arman <mr.kazemi.ar@gmail.com>
2022-01-10 00:54:03 +0330

    fix : indent bug

--- core/manager.py
+++ core/manager.py
@@ -12,38 +12,34 @@ class BaseManager(ABC):
     @classmethod
     @abstractmethod
     def create(cls, *args):
-    """
-    to create an object and save into data base
-    """
+        """
+        to create an object and save into data base
+        """
 
+    @classmethod
+    @abstractmethod
+    def read(cls, row_id):
+        """
+        to read data from database
+        """
 
-@classmethod
-@abstractmethod
-def read(cls, row_id):
-    """
-    to read data from database
-    """
+    @classmethod
+    @abstractmethod
+    def update(cls, column_name, row_id, value):
+        """
+        to update data in database
+        """
 
+    @classmethod
+    @abstractmethod
+    def delete(cls, row_id):
+        """
+        to delete data from database
+        """
 
-@classmethod
-@abstractmethod
-def update(cls, column_name, row_id, value):
-    """
-    to update data in database
-    """
-
-
-@classmethod
-@abstractmethod
-def delete(cls, row_id):
-    """
-    to delete data from database
-    """
-
-
-@classmethod
-@abstractmethod
-def read_all(cls):
-    """
-    to read all data from one table
-    """
+    @classmethod
+    @abstractmethod
+    def read_all(cls):
+        """
+        to read all data from one table
+        """
