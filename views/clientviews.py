b47a56d90be0fb4b89229e93a24cd9e1c5340b53
arman <mr.kazemi.ar@gmail.com>
2022-01-09 18:57:20 +0330

    fix : some name

--- views/clientviews.py
+++ views/clientviews.py
@@ -27,8 +27,8 @@ def about():
 
 def menu():
     if request.method == 'GET':
-        category_dict = category.CategoryModels.all_categories()
-        menu_dict = menu_items.MenuItems.all_menu_item()
+        category_dict = category.CategoryModels.read_all()
+        menu_dict = menu_items.MenuItems.read_all()
         data = base_variables
         data['page']['title'] = "menu page !"
         return render_template('Customer/menu.html', category_dict=category_dict, menu_dict=menu_dict,
@@ -43,12 +43,12 @@ def team():
 
 def order():
     if request.method == "POST":
-        menu_dict = list(menu_items.MenuItems.all_menu_item().keys())
+        menu_dict = list(menu_items.MenuItems.read_all().keys())
         req = request.form.get
         for i in menu_dict:
             if req(i) != 0:
                 orders.Order(req('table'), req(str(i)))
-                order_dict = list(orders.Order.all_orders().keys())
+                order_dict = list(orders.Order.read_all().keys())
                 o = order_dict[len(order_dict) - 1]
                 order_item.OrderItem(o, i)
         return render_template('Customer/home.html')
