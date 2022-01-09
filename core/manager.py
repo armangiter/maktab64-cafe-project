a055c226c0ddfa70c5308d16576cd6933f843ef8
arman <mr.kazemi.ar@gmail.com>
2022-01-09 17:01:33 +0330

    feat : import files

---
+++ core/manager.py
@@ -0,0 +1,4 @@
+from sqlalchemy import Column, ForeignKey, String, create_engine, Integer, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
