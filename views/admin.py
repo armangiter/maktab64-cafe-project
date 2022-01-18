e99529a117d6e2473a29df83cd5421a299d2f562
arman <mr.kazemi.ar@gmail.com>
2022-01-18 11:00:56 +0330

    fix : top_five reciept cost

--- views/admin.py
+++ views/admin.py
@@ -121,4 +121,10 @@ def tables():
 
 def dashboard():
     if request.method == 'GET':
+        reciept_dict = reciepts.Receipt.read_all()f
+        top_five = []
+        for r in reciept_dict:
+            top_five.append(reciept_dict[r]['total_price'])
+        top_five.sort()
+        print(top_five)
         return render_template('cashier/dashbord.html')
